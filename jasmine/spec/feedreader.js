0e86e600ecc66c65a0fdefb87cba1a5cc5136bf6
pnellesen <pnellesen@hotmail.com>
2015-05-06 20:49:42 -0500

    Added tests for feed urls and names

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,12 +31,23 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it('have urls defined and urls are not empty', function() {
+        	for (var i = 0; i < allFeeds.length; i++) {
+        		expect(allFeeds[i].url).toBeDefined();
+        		expect(allFeeds[i].url.length).toBeGreaterThan(0);
+        	}
+        });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('have names defined and names are not empty', function() {
+        	for (var i = 0; i < allFeeds.length; i++) {
+        		expect(allFeeds[i].name).toBeDefined();
+        		expect(allFeeds[i].name.length).toBeGreaterThan(0);
+        	}
+        });
     });
 
 
